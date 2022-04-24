852635a85df5149cdffb8d5fd52cb58c68ddb64d
HClarke26 <harryclarke26@gmail.com>
2022-04-24 14:47:41 +0100

    filling out game methods

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -1,3 +1,55 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase('Better late than never'),
+            new Phrase('Beat around the bush'),
+            new Phrase('Hit the sack'),
+            new Phrase('Miss the boat'),
+            new Phrase('The best of both worlds')
+        ];
+        this.activePhrase = null;
+    }
+
+    getRandomPhrase() {
+        var randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        return randomPhrase
+    }
+
+    startGame() {
+        const overlay = document.getElementById("overlay");
+        overlay.style.display = 'none';
+
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    
+    }
+
+    checkForWin() {
+        const letters = document.querySelectorAll('.letter');
+        const shown = document.querySelectorAll('.show')
+        
+        if(shown.length === letters.length){
+            return true;
+        } else { 
+            return false;
+        }
+    }
+
+    removeLife(){
+        const liveHearts = document.querySelectorAll('.tries img');
+        liveHearts[this.missed].src = 'images/lostHeart.png';
+        this.missed += 1;
+        if(this.missed > 4){
+            this.gameOver();
+        }
+    };
+    
+}
+
+
+
--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,43 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+        const phraseDiv = document.getElementById("phrase");
+        const ul = phraseDiv.querySelector("ul");
+
+        for(let i = 0; i < this.phrase.length; i++){
+          
+            if(this.phrase[i] !== " "){
+                ul.innerHTML += `<li class="hide letter">${this.phrase[i]}</li>`;
+            } else {
+                ul.innerHTML += `<li class="space">${this.phrase[i]}</li>`;
+            }
+        }
+        return this.phrase;
+    }
+
+    checkLetter(letter) {
+        if (this.phrase.includes(letter)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    showMatchedLetter(letter) {
+        let letters = ul.children;
+        for(let i = 0; i < letters.length; i++){
+            if(letter === letters[i].textContent){
+                letters[i].classList.remove('hide');
+                letters[i].classList.add('show');
+            }
+        }
+
+    }
+}
